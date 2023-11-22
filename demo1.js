e48ab52ddb5e7ac791bb64f5ed2c398de7f5493b
ljcaffot <leeancaffot@gmail.com>
2023-11-22 18:42:52 -0300

    hm 03 done

---
+++ demo1.js
@@ -0,0 +1,36 @@
+function sumar(a,b) {
+    // a = 10, b = 24
+    var operacion = a + b
+    // operacion = 34
+    return operacion
+} 
+var resultado = sumar(10,24)
+console.log (resultado)
+
+
+
+if(2 < 4){
+    console.log("2 es menor que 4")
+}else if (2 < 6 ){
+    console.log("2 es menor que 6")
+}else{
+    console.log("No se cumplió nada")
+}
+
+
+if(2 < 1){
+    console.log("2 es menor que 4")
+}else if (2 < 6 ){
+    console.log("2 es menor que 6")
+}else{
+    console.log("No se cumplió nada")
+}
+
+if(2 < 1){
+    console.log("2 es menor que 4")
+}else if (2 < 0 ){
+    console.log("2 es menor que 6")
+}else{
+    console.log("No se cumplió nada")
+}
+
